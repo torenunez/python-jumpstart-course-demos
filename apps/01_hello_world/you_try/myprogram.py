c9f2f220af845a46d019909e6a72d8473e3fbf30
torenunez <tore.nunez@gmail.com>
2016-11-09 21:48:15 -0600

    through app 3

---
+++ apps/01_hello_world/you_try/myprogram.py
@@ -0,0 +1,8 @@
+print('--------------------------------------------------------')
+print('                  HELLO APP')
+print('--------------------------------------------------------')
+print()
+
+name = input('What is your name? ')
+greeting = 'Nice to meet you ' + name
+print(greeting)
