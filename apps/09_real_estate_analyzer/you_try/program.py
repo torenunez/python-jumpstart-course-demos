0ed10c4e570f129042ec01c2da5ea87ead90302f
torenunez <tore.nunez@gmail.com>
2016-12-01 09:36:15 -0600

    app 9 generator expressions

--- apps/09_real_estate_analyzer/you_try/program.py
+++ apps/09_real_estate_analyzer/you_try/program.py
@@ -3,8 +3,7 @@ import os
 try:
     import statistics
 except:
-    # error code instead
-    import statistics_standin_for_py2 as statistics
+    import stats_for_py2 as statistics
 
 from data_types import Purchase
 
@@ -17,10 +16,10 @@ def main():
 
 
 def print_header():
-    print('----------------------------------')
-    print('  REAL ESTATE DATA MINING APP')
-    print('----------------------------------')
-    print()
+    print('--------------------------------------')
+    print('     REAL ESTATE DATA MINING APP')
+    print('--------------------------------------')
+    print('')
 
 
 def get_data_file():
@@ -29,18 +28,31 @@ def get_data_file():
                         'SacramentoRealEstateTransactions2008.csv')
 
 
+
 def load_file(filename):
     with open(filename, 'r', encoding='utf-8') as fin:
-    # with open(filename, 'r') as fin:
+
         reader = csv.DictReader(fin)
         purchases = []
         for row in reader:
-            p = Purchase.create_from_dict()
+            # print(type(row), row)
+            # bed_count = row['beds']
+            # print('Bed count: {}, type: {}'.format(row['beds'], type(row['beds'])))
+            p = Purchase.create_from_dict(row)
             purchases.append(p)
 
+        # print(purchases[0].__dict__)
         return purchases
 
+        # option 2
+        # header = fin.readline().strip()
+        # reader = csv.reader(fin)
+        # for row in reader:
+        #     print(type(row), row)
+        #     bed_count = row[4]
 
+
+# option 1
 # def load_file_basic(filename):
 #     with open(filename, 'r', encoding='utf-8') as fin:
 #         header = fin.readline().strip()
@@ -53,74 +65,73 @@ def load_file(filename):
 #             lines.append(line_data)
 #
 #         print(lines[:5])
-# list[Purchase]
 
 
-# def get_price(p):
-#     return p.price
 
-def query_data(data): # list[Purchase]):
-    
+def get_price(p):
+    return p.price
+
+def query_data(data):
+
+    # if data was sorted by price:
     # data.sort(key=get_price)
     data.sort(key=lambda p: p.price)
 
+    # most expensive house?
     high_purchase = data[-1]
-    print("The most expensive house is ${:,} with {} beds and {} baths".format(
+    print('The most expensive house is ${:,}, with {} beds and {} baths'.format(
         high_purchase.price, high_purchase.beds, high_purchase.baths))
 
+    # least expensive house?
     low_purchase = data[0]
-    print("The least expensive house is ${:,} with {} beds and {} baths".format(
+    print('The lease expensive house is ${:,}, with {} beds and {} baths'.format(
         low_purchase.price, low_purchase.beds, low_purchase.baths))
 
     # average price house?
-    # prices = list()  # []
-    # for pur in data:
-    #     prices.append(pur.price)
+    # prices = []
+    # for pr in data:
+    #     prices.append(pr.price)
 
-    prices = (
-        p.price  # projection or items
-        for p in data  # the set to process
-    )
+    prices = [
+        p.price # projection or items
+        for p in data # the set to process
+    ]
+
 
     ave_price = statistics.mean(prices)
-    print("The average home price is ${:,}".format(int(ave_price)))
+    print('The average home price is ${:,}'.format(int(ave_price)))
 
     # average price of 2 bedroom houses
     # prices = []
-    # baths = []
-    # for pur in data:
-    #     if pur.beds == 2:
-    #         prices.append(pur.price)
+    # for pr in data:
+    #     if pr.beds ==2:
+    #         prices.append(pr.price)
 
     two_bed_homes = (
-        p  # projection or items
-        for p in data  # the set to process
-        if announce(p, '2-bedrooms, found {}'.format(p.beds)) and p.beds == 2 # test / condition
+        p # projection or items
+        for p in data # the set to process
+        if p.beds == 2
     )
+
+    # print(two_bed_homes)
 
     homes = []
     for h in two_bed_homes:
-        if len(homes) > 5:
-            break
+        # if len(homes)>5:
+        #     break
         homes.append(h)
 
-    ave_price = statistics.mean((announce(p.price, 'price') for p in homes))
+    # print(homes)
+
+    ave_price = statistics.mean((p.price for p in homes))
     ave_baths = statistics.mean((p.baths for p in homes))
     ave_sqft = statistics.mean((p.sq__ft for p in homes))
     print("Average 2-bedroom home is ${:,}, baths={}, sq ft={:,}"
           .format(int(ave_price),round(ave_baths, 1), round(ave_sqft, 1)))
 
 
-def announce(item, msg):
-    print("Pulling item {} for {}".format(item, msg))
-    return item
+
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
